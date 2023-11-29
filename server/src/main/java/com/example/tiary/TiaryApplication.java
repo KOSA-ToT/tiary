0cfff404aff91dd7753de0a08a9c33011395a0fb
YSHeee <scotchbroom0@kangwon.ac.kr>
2023-11-29 12:13:31 +0900

    init: first commit

---
+++ server/src/main/java/com/example/tiary/TiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiaryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/tiary/TiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
