423b4783cca695fedd352ca2c8161e1dcfb040f5
Nohkii <120151129+Nohkii@users.noreply.github.com>
2023-12-05 10:16:43 +0900

    [SCRUM-19]-feat: comment service 구현
    - SCRUM-26-댓글 작성
    - SCRUM-29-댓글 수정
    - SCRUM-37-댓글 조회
    - SCRUM-40-댓글 삭제
    
    `comment` 기능 구현을 위한 service 구현

--- server/src/main/java/com/example/tiary/comment/service/CommentService.java
+++ server/src/main/java/com/example/tiary/comment/service/CommentService.java
@@ -1,6 +1,8 @@
 package com.example.tiary.comment.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,21 +28,43 @@ public class CommentService {
 
 	//저장
 	@Transactional
-	public Comment save(CommentRequestDTO commentRequestDTO, Long articleId) {
+	public Comment create(CommentRequestDTO commentRequestDTO, Long articleId) {
 		Article article = articleRepository.findById(articleId)
 			.orElseThrow(() -> new EntityNotFoundException("게시물이 존재하지 않습니다."));
 
 		return commentRepository.save(commentRequestDTO.toEntity(article));
 	}
 
+	// 전체 댓글
 	@Transactional(readOnly = true) // 직관적으로 이거는 조회하는 메서드임을 알려줌
-	public List<CommentResponseDTO> readAll() {
-		List<Comment> commentList = commentRepository.findAll();
-		List<CommentResponseDTO> commentResponseDTOList = null;
-		CommentResponseDTO commentResponseDTO = null;
+	public List<CommentResponseDTO> readCommentList(Long articleId) {
+		List<Comment> commentList = commentRepository.findAllByArticleId(articleId);
+		List<CommentResponseDTO> commentResponseDTOList = new ArrayList<>();
+		CommentResponseDTO commentResponseDTO = new CommentResponseDTO();
 		for (Comment comment : commentList) {
-			commentResponseDTOList.add(commentResponseDTO.from(comment));
+			commentResponseDTOList.add(new CommentResponseDTO().from(comment));
 		}
 		return commentResponseDTOList;
+	}
+
+	// 수정
+	@Transactional
+	public CommentResponseDTO update(Long commentId, CommentRequestDTO commentRequestDTO) {
+		Comment comment = commentRepository.findById(commentId).get();
+		// if(!commentRequestDTO.getContent().isBlank()){
+		// 	comment.updateContent(commentRequestDTO.getContent());
+		// }
+		Optional.ofNullable(commentRequestDTO.getContent()).ifPresent(comment::updateContent);
+
+		CommentResponseDTO commentResponseDTO = new CommentResponseDTO();
+		return commentResponseDTO.from(commentRepository.save(comment));
+	}
+
+	// 삭제
+	public String delete(Long commentId) {
+		Comment comment = commentRepository.findById(commentId)
+			.orElseThrow(() -> new EntityNotFoundException("댓글이 이미 삭제되었습니다."));
+		commentRepository.deleteById(commentId);
+		return "삭제 완료";
 	}
 }
