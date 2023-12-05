f89e744687182e0eccf5931340092235b128411b
Nohkii <120151129+Nohkii@users.noreply.github.com>
2023-12-05 10:17:47 +0900

    [SCRUM-19]-feat: comment repository 구현
    - SCRUM-37-댓글 조회
    
    `comment` 기능 구현을 위한 repository 구현

--- server/src/main/java/com/example/tiary/comment/repository/CommentRepository.java
+++ server/src/main/java/com/example/tiary/comment/repository/CommentRepository.java
@@ -1,9 +1,12 @@
 package com.example.tiary.comment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.tiary.comment.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+	public List<Comment> findAllByArticleId(Long articleId);
 
 }
