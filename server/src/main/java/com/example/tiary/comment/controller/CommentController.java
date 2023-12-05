f677a274f6ef5d0dbded085089214fe1771c736c
Nohkii <120151129+Nohkii@users.noreply.github.com>
2023-12-05 20:08:33 +0900

    [SCRUM-19]-feat: comment controller구현
    - SCRUM-26-댓글 작성
    - SCRUM-29-댓글 수정
    - SCRUM-37-댓글 조회
    - SCRUM-40-댓글 삭제
    - SCRUM-44-비회원 댓글 작성
    - SCRUM-45-비회원 댓글 조회
    - SCRUM-46-비회원 댓글 수정
    - SCRUM-47-비회원 댓글 삭제
    
    `comment` 기능 구현을 위한 controller 구현

--- server/src/main/java/com/example/tiary/comment/controller/CommentController.java
+++ server/src/main/java/com/example/tiary/comment/controller/CommentController.java
@@ -2,7 +2,6 @@ package com.example.tiary.comment.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,35 +23,48 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/comment")
 @RestController
 public class CommentController {
-	@Autowired
-	CommentService commentService;
+	private final CommentService commentService;
 
+	public CommentController(CommentService commentService) {
+		this.commentService = commentService;
+	}
+
+	// 댓글 등록
 	@PostMapping("/{article-id}")
 	public ResponseEntity create(@PathVariable("article-id") Long articleId,
 		@RequestBody CommentRequestDTO commentRequestDTO) {
-
 		return new ResponseEntity<>(commentService.create(commentRequestDTO, articleId), HttpStatus.OK);
 	}
 
+	// 전체 댓글 가져오기
 	@GetMapping("/{article-id}")
 	public ResponseEntity<List<CommentResponseDTO>> readCommentList(@PathVariable("article-id") Long articleId) {
 		List<CommentResponseDTO> commentResponseDTOList = commentService.readCommentList(articleId);
-
 		return ResponseEntity.status(HttpStatus.CREATED).body(commentResponseDTOList);
 	}
 
+	// 익명 댓글 비밀번호 확인
+	@PostMapping("/password-confirm/{comment-id}")
+	public ResponseEntity confirmPassword(@PathVariable("comment-id") Long commentId,
+		@RequestBody CommentRequestDTO commentRequestDTO) {
+		if (commentService.confirmPassword(commentId, commentRequestDTO.getPassword())) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	// 회원 댓글 수정
 	@PatchMapping("/{comment-id}")
 	public ResponseEntity<CommentResponseDTO> updateComment(
 		@PathVariable("comment-id") Long commentId, @RequestBody CommentRequestDTO commentRequestDTO) {
-
 		return ResponseEntity.status(HttpStatus.RESET_CONTENT)
 			.body(commentService.update(commentId, commentRequestDTO));
 	}
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity deleteComment(@PathVariable ("id") Long commentId) {
-
+	// 댓글 삭제
+	@DeleteMapping("/{comment-id}")
+	public ResponseEntity deleteComment(@PathVariable("comment-id") Long commentId) {
 		return ResponseEntity.status(HttpStatus.RESET_CONTENT).body(commentService.delete(commentId));
 	}
-
 }
