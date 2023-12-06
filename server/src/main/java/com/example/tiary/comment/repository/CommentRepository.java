ab9f89cde2dcbc7fd953b9ab44c434b2e411d447
Nohkii <120151129+Nohkii@users.noreply.github.com>
2023-12-06 18:20:12 +0900

    [SCRUM-19]-feat: 대댓글 기능 위한 repository 수정
    - SCRUM-42-대댓글 기능
    
    대댓글 기능 구현을 위해 repository 수정

--- server/src/main/java/com/example/tiary/comment/repository/CommentRepository.java
+++ server/src/main/java/com/example/tiary/comment/repository/CommentRepository.java
@@ -1,12 +1,9 @@
 package com.example.tiary.comment.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.tiary.comment.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	public List<Comment> findAllByArticleId(Long articleId);
-
+	List<Comment> findAllByArticleIdOrderByParentIdAscCreatedAtAsc(Long articleId);
 }
