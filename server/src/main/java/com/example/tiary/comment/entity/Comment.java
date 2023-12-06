2092e87718ab93e439cf4372a15ef2a96e8d54a6
Nohkii <120151129+Nohkii@users.noreply.github.com>
2023-12-06 18:19:32 +0900

    [SCRUM-19]-feat: 대댓글 기능 위한 entity 수정
    - SCRUM-42-대댓글 기능
    
    대댓글 기능 구현을 위해 entity 수정

--- server/src/main/java/com/example/tiary/comment/entity/Comment.java
+++ server/src/main/java/com/example/tiary/comment/entity/Comment.java
@@ -1,14 +1,25 @@
 package com.example.tiary.comment.entity;
 
+import static jakarta.persistence.FetchType.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
 import com.example.tiary.article.entity.Article;
 import com.example.tiary.global.audit.AuditingFields;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +30,7 @@ import lombok.ToString;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
 @ToString
 @Getter
 @Entity
@@ -30,6 +42,7 @@ public class Comment extends AuditingFields {
 	@Column(length = 200)
 	private String content;
 
+	@Column(length = 20)
 	private String password;
 
 	@Column(name = "comment_type")
@@ -38,12 +51,37 @@ public class Comment extends AuditingFields {
 	@ManyToOne
 	private Article article;
 
+	@ColumnDefault("FALSE")
+	@Column(nullable = false)
+	private Boolean isDeleted;
+
+	@JsonIgnore
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "parent_id")
+	@ToString.Exclude
+	private Comment parent;
+
+	@OneToMany(mappedBy = "parent", orphanRemoval = true) // 부모댓글이 삭제되면 자식댓글도 삭제 (DB도 삭제)
+	@ToString.Exclude
+	private List<Comment> children = new ArrayList<>();
+
+	public void setParent(Comment parent) {
+		this.parent = parent;
+	}
+
 	//    @ManyToOne
-	//    @JoinColumn(name = "user_id")
+	//    @JoinColumn(name = "us!er_id")
 	//    private Users users;
 
 	public void updateContent(String content) {
 		this.content = content;
 	}
 
+	public void updateParent(Comment comment) {
+		this.parent = comment;
+	}
+
+	public void changeIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
 }
