4799d86c608776ff587f18bc43d01922e5677327
ckaanf <skywlstn777@gmail.com>
2023-12-07 15:07:04 +0900

    [SCRUM-81] refactoring: 조회수 증가 반영
    조회수 증가 반영을 위해 리팩토링
    중복 방지는 하지 않음

--- server/src/main/java/com/example/tiary/article/repository/ArticleRepository.java
+++ server/src/main/java/com/example/tiary/article/repository/ArticleRepository.java
@@ -3,10 +3,17 @@ package com.example.tiary.article.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.tiary.article.entity.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 	List<Article> findAllByCategory_CategoryCode(String categoryCode);
+
+	@Modifying
+	@Query("update Article a set a.view = a.view +1 where a.id = :id")
+	int updateViews(@Param("id")Long articleId);
 }
