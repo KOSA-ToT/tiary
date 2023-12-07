97363d4336bc890f072931e22e949301f92eda4e
rlrl9 <klala8@naver.com>
2023-12-07 12:36:48 +0900

    [SCRUM-34]-feat:구독 목록

--- server/src/main/java/com/example/tiary/myPage/repository/SubscribeRepository.java
+++ server/src/main/java/com/example/tiary/myPage/repository/SubscribeRepository.java
@@ -1,12 +1,23 @@
 package com.example.tiary.myPage.repository;
 
+import com.example.tiary.myPage.dto.response.ResponseSubscribeDto;
 import com.example.tiary.myPage.entity.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
-    List<Subscribe> findByWriterId(Long writerId);
+    List<Subscribe> findByWriterId(@Param("writerId") Long writerId);
 
-    List<Subscribe> findByUserId(Long userId);
+    List<Subscribe> findByUserId(@Param("userId") Long userId);
+
+    //구독자 조회
+    @Query("SELECT new com.example.tiary.myPage.dto.response.ResponseSubscribeDto(s.userId, s.writerId, u.nickname, u.userPicture) FROM Subscribe s LEFT JOIN Users u ON s.userId = u.id WHERE s.writerId = :writerId")
+    List<ResponseSubscribeDto> showSubscribeList(@Param("writerId") Long writerId);
+
+    //구독 작가 조회
+    @Query("SELECT new com.example.tiary.myPage.dto.response.ResponseSubscribeDto(s.userId, s.writerId, u.nickname, u.userPicture) FROM Subscribe s LEFT JOIN Users u ON s.writerId = u.id WHERE s.userId = :userId")
+    List<ResponseSubscribeDto> showSubscribedWriterList(@Param("userId") Long userId);
 }
--- server/src/main/java/com/example/tiary/myPage/service/impl/SubscribeServiceImpl.java
+++ server/src/main/java/com/example/tiary/myPage/service/impl/SubscribeServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.tiary.myPage.service.impl;
 
-import com.example.tiary.article.dto.response.ResponseArticleDto;
 import com.example.tiary.myPage.dto.response.ResponseSubscribeDto;
 import com.example.tiary.myPage.entity.Subscribe;
 import com.example.tiary.myPage.repository.SubscribeRepository;
+import com.example.tiary.myPage.repository.UserRepository;
 import com.example.tiary.myPage.service.SubscribeService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,23 +13,29 @@ import java.util.List;
 @Service
 public class SubscribeServiceImpl implements SubscribeService {
     private final SubscribeRepository subscribeRepository;
+    private final UserRepository userRepository;
 
-    public SubscribeServiceImpl(SubscribeRepository subscribeRepository) {
+    public SubscribeServiceImpl(SubscribeRepository subscribeRepository, UserRepository userRepository) {
         this.subscribeRepository = subscribeRepository;
+        this.userRepository = userRepository;
     }
 
     //구독 작가의 구독자 조회
     @Transactional(readOnly = true)
     @Override
     public List<ResponseSubscribeDto> readSubscriber(Long writerId){
-        List<Subscribe> subscribeList = subscribeRepository.findByWriterId(writerId);
-        return subscribeList.stream().map(ResponseSubscribeDto::from).toList();
+//        List<Subscribe> subscribeList = subscribeRepository.findByWriterId(writerId);
+        List<ResponseSubscribeDto> subscriberList =  subscribeRepository.showSubscribeList(writerId);
+//        return subscribeList.stream().map(ResponseSubscribeDto::from).toList();
+        return subscriberList;
     }
     //구독자의 구독 작가 조회
     @Transactional(readOnly = true)
     @Override
     public List<ResponseSubscribeDto> readsubscribedWriter(Long userId){
-        List<Subscribe> subscribeList = subscribeRepository.findByUserId(userId);
-        return subscribeList.stream().map(ResponseSubscribeDto::from).toList();
+//        List<Subscribe> subscribeList = subscribeRepository.findByUserId(userId);
+//        return subscribeList.stream().map(ResponseSubscribeDto::from).toList();
+        List<ResponseSubscribeDto> subscribedWriterList =  subscribeRepository.showSubscribedWriterList(userId);
+        return subscribedWriterList;
     }
 }
