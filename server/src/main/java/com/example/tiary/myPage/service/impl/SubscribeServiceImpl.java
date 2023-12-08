36b7ad2ea8af969901332ebc28d79633530afb02
rlrl9 <klala8@naver.com>
2023-12-08 18:06:42 +0900

    [SCRUM-76,77] feat: 구독,구취

--- server/src/main/java/com/example/tiary/myPage/service/impl/SubscribeServiceImpl.java
+++ server/src/main/java/com/example/tiary/myPage/service/impl/SubscribeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.tiary.myPage.service.impl;
 
+import com.example.tiary.article.entity.Article;
 import com.example.tiary.myPage.dto.response.ResponseSubscribeDto;
 import com.example.tiary.myPage.entity.Subscribe;
 import com.example.tiary.myPage.repository.SubscribeRepository;
@@ -37,5 +38,31 @@ public class SubscribeServiceImpl implements SubscribeService {
 //        return subscribeList.stream().map(ResponseSubscribeDto::from).toList();
         List<ResponseSubscribeDto> subscribedWriterList =  subscribeRepository.showSubscribedWriterList(userId);
         return subscribedWriterList;
+    }
+    //구독하기
+    @Transactional
+    @Override
+    public boolean addSubscribe(Long writerId, Long userId){
+        try {
+            Subscribe subscribe = new Subscribe(userId, writerId);
+            subscribeRepository.save(subscribe);
+            return true; // 저장 성공 시 true 반환
+        } catch (Exception e) {
+            // 예외 발생 시 처리
+            return false;
+        }
+    }
+    //구독 취소하기
+    @Transactional
+    @Override
+    public boolean cancelSubscribe(Long writerId, Long userId){
+        try {
+            Subscribe subscribe = new Subscribe(userId, writerId);
+            subscribeRepository.delete(subscribe);
+            return true; // 삭제 성공 시 true 반환
+        } catch (Exception e) {
+            // 예외 발생 시 처리
+            return false;
+        }
     }
 }
