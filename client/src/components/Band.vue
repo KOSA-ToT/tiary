a3a03a62b70bd755146b4dfab65b13ebc6abdf42
YSHeee <scotchbroom0@kangwon.ac.kr>
2023-12-11 14:49:12 +0900

    [SCRUM-95] design: Home 화면 구성

---
+++ client/src/components/Band.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="two" class="wrapper style3">
+      <div class="inner">
+        <header class="align-center">
+          <p>
+            Nam vel ante sit amet libero scelerisque facilisis eleifend vitae
+            urna
+          </p>
+          <h2>Morbi maximus justo</h2>
+        </header>
+      </div>
+    </section>
+</template>
+<style scoped>
+@import "@/assets/css/main.css";
+</style>
