15f3a89f9ed37ed162580c508bfc5a006a887c10
YSHeee <scotchbroom0@kangwon.ac.kr>
2023-12-12 10:11:35 +0900

    [SCRUM-25] feat: 로그인/회원가입 모달 구현

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,12 +1,36 @@
 <template>
+    <button @click="openModal('로그인')">로그인 버튼</button>
+    <button @click="openModal('회원가입')">회원가입 버튼</button>
     <Thumbnails></Thumbnails>
     <Band></Band>
     <Card></Card>
     <Footer></Footer>
+    <UserModal
+        v-if="userVars.isShowModal"
+        v-model:task="userVars.task"
+        v-model:isShowModal="userVars.isShowModal"
+        >
+    </UserModal>
 </template>
+
 <script setup>
+import {ref } from 'vue'
+
 import Footer from '@/components/Footer.vue'
 import Thumbnails from '@/components/Thumbnails.vue'
 import Card from '@/components/Card.vue'
 import Band from '@/components/Band.vue'
+import UserModal from '@/components/UserModal.vue'
+
+// 유저
+const userVars = ref({
+    task: '',
+    isShowModal: false
+})
+
+function openModal(tasks) {
+    userVars.value.task = tasks
+    userVars.value.isShowModal = true;
+}
+
 </script>
