c9b01939325905c70f3c0a7867705a1865c7c58f
rlrl9 <klala8@naver.com>
2023-12-13 09:27:16 +0900

    [SCRUM-112] feat: 구독자, 구독 작가 조회

--- client/src/components/myPage/myInfo.vue
+++ client/src/components/myPage/myInfo.vue
@@ -23,7 +23,7 @@
                             <button type="button" @click="editEmail()"
                                 class="rounded-md bg-white px-2.5 py-1.5 text-sm font-normal text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">변경</button>
                         </div>
-                        <p v-if="isChanged">{{msgEmail}}</p>
+                        <p v-if="isChanged">{{ msgEmail }}</p>
                     </div>
 
                 </div>
@@ -52,25 +52,78 @@
                 <div class="pl-3 col-span-full">
                     <label for="photo" class="block text-sm font-medium leading-6 text-gray-900">프로필 이미지</label>
                     <div class="flex items-center mt-2 gap-x-3">
-                        <svg class="w-12 h-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd"
-                                d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                                clip-rule="evenodd" />
-                    </svg>
-                    <button type="button"
-                        class="rounded-md bg-white px-2.5 py-1.5 text-sm font-normal text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">변경하기</button>
+                        <input type="file" @change="handleFileChange" style="display: none" ref="fileInput" />
+                        <div @click="$refs.fileInput.click()">
+                            <svg v-if="!previewImage" class="w-12 h-12 text-gray-300 cursor-pointer" viewBox="0 0 24 24"
+                                fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                                    clip-rule="evenodd" />
+                            </svg>
+
+                            <!-- 이미지 미리보기 -->
+                            <img v-else :src="previewImage" alt="Preview"
+                                class="w-12 h-12 text-gray-300 rounded-full cursor-pointer" style="object-fit: cover;" />
+                        </div>
+                        <button type="button" @click="changeProfileImg"
+                            class="rounded-md bg-white px-2.5 py-1.5 text-sm font-normal text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">변경하기</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div @click="modalOpen = true">
+            <h2 class="text-base font-semibold leading-7 text-gray-900"><u>Tiary</u>에서 탈퇴하기</h2>
+        </div>
+        <div class="flex items-center justify-center h-screen">
+            <div v-if="modalOpen" class="fixed inset-0 transition-opacity" aria-hidden="true" @click="modalOpen = false">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+
+            <div v-if="modalOpen" class="fixed inset-0 z-10 overflow-y-auto">
+                <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+                    <div class="inline-block w-full overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-sm sm:w-full"
+                        role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                        <div class="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+                            <div class="sm:flex sm:items-start">
+                                <div
+                                    class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                                    <svg width="64px" height="64px" class="w-6 h-6 text-red-600" stroke="currentColor"
+                                        fill="none" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg"
+                                        stroke-width="0.45600000000000007">
+                                        <!-- ... (SVG paths) ... -->
+                                    </svg>
+                                </div>
+                                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <div class="flex">
+                                        <h3 class="text-lg font-semibold leading-6 text-gray-900" id="modal-headline">정말로
+                                            탈퇴하시겠습니까?</h3>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-1">
+                                            <!-- ... (SVG paths) ... -->
+                                        </svg>
+                                    </div>
+                                    <div class="mt-2">
+                                        <p class="ml-3 text-sm text-gray-500">탈퇴 후에 번복할 수 없습니다.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="justify-center px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+                            <button @click="handleWithdrawal" type="button"
+                                class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-red-500 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">탈퇴</button>
+                            <button @click="modalOpen = false" type="button"
+                                class="inline-flex justify-center w-full px-4 py-2 mt-3 mr-20 text-base font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">취소</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div @click="handleWithdrawal">
-        <h2 class="text-base font-semibold leading-7 text-gray-900"><u>Tiary</u>에서 탈퇴하기</h2>
-    </div>
-</div></template>
+</template>
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
 
 const props = defineProps({
     user: {
@@ -83,21 +136,29 @@ let msgEmail = ref("");
 let msgNickname = ref("");
 let isChanged = ref(false);
 let isChangedNickname = ref(false);
+const modalOpen = ref(false);
+const route = useRoute();
+const router = useRouter();
+const previewImage = ref(null);
+
+// function modalOpen() {
+//     modalCheck = !modalCheck;
+// }
 async function editEmail() {
     await axios
         .patch("http://localhost:8088/users/1/updateEmail", {
             email: props.user.email.value,
         })
         .then((response) => {
-            isChanged.value=true;
+            isChanged.value = true;
             // changedNickName.value = response.data.nickname;
             // console.log(changedNickName.value);
-            if(response.status === 205){
-                msgEmail.value="변경되었습니다.";
+            if (response.status === 205) {
+                msgEmail.value = "변경되었습니다.";
             }
-            console.log("status: "+response.status);
-            console.log("data : "+response.data);
-            console.log("isChanged: "+isChanged.value);
+            console.log("status: " + response.status);
+            console.log("data : " + response.data);
+            console.log("isChanged: " + isChanged.value);
         })
         .catch(error => {
             // alert(error);
@@ -113,11 +174,11 @@ async function editNickname() {
             nickname: props.user.nickname.value,
         })
         .then((response) => {
-            isChangedNickname.value=true;
+            isChangedNickname.value = true;
             // changedNickName.value = response.data.nickname;
             // console.log(changedNickName.value);
-            if(response.status === 205){
-                msgNickname.value="변경되었습니다.";
+            if (response.status === 205) {
+                msgNickname.value = "변경되었습니다.";
             }
         })
         .catch(error => {
@@ -129,16 +190,56 @@ async function editNickname() {
 }
 async function handleWithdrawal() {
     await axios
-        .patch("http://localhost:8088/users/1/inactive",{
+        .patch("http://localhost:8088/users/1/inactive", {
             nickname: props.user.nickname.value,
         })
-        .then((response)=>{
+        .then((response) => {
             console.log("inactive");
+            modalOpen.value = false;
         })
-        .catch(error=>{
+        .catch(error => {
             console.log("에러");
         }
         );
     console.log('클릭 이벤트 발생!');
+    router.push("/");
+};
+async function changeProfileImg() {
+    await axios
+        .patch("http://localhost:8088/users/1/uploadProfileImg", {
+            nickname: props.user.nickname.value,
+        })
+        .then((response) => {
+            // isChangedNickname.value = true;
+            // // changedNickName.value = response.data.nickname;
+            // // console.log(changedNickName.value);
+            // if (response.status === 205) {
+            //     msgNickname.value = "변경되었습니다.";
+            // }
+        })
+        .catch(error => {
+            // if (error.response && error.response.status === 409) {
+            //     isChangedNickname.value = true;
+            //     msgNickname.value = "이미 있는 닉네임입니다.";
+            // }
+        });
+};
+
+
+// 파일 선택 이벤트 핸들러
+const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        // 이미지 미리보기 업데이트
+        const reader = new FileReader();
+        reader.onload = () => {
+            previewImage.value = reader.result;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        // 파일 선택이 해제된 경우 미리보기 초기화
+        previewImage.value = null;
+    }
 };
 </script>
--- client/src/components/myPage/mySubscriber.vue
+++ client/src/components/myPage/mySubscriber.vue
@@ -1,11 +1,11 @@
 <template>
-    <SubscribeTab></SubscribeTab>
-    <div class="flex flex-col justify-center items-center h-[100vh] pt-4">
+    <SubscribeTab @subscribe="listSubscriber"></SubscribeTab>
+    <div class="flex flex-col h-[100vh] pt-4">
         <div
             class="relative flex flex-col items-center rounded-[10px] border-[1px] border-gray-200 w-[576px] mx-auto p-4 bg-white bg-clip-border shadow-md shadow-[#F3F3F3] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
             <div class="flex items-center justify-between w-full p-3 rounded-t-3xl">
             </div>
-            <div
+            <div v-for="subscriber in subscriberList"
                 class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
                 <div class="flex items-center gap-3">
                     <div class="flex items-center justify-center w-16 h-16">
@@ -15,12 +15,12 @@
                     </div>
                     <div class="flex flex-col">
                         <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                            sky123
+                            {{ subscriber.nickname }}
                         </h5>
                     </div>
                 </div>
                 <div class="flex items-center justify-center mt-3 text-navy-700 dark:text-white">
-                    <button
+                    <button @click="subscribe(subscriber.userId)"
                         class="flex linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-semibold text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
@@ -32,109 +32,67 @@
 
                 </div>
             </div>
-            <div
-                class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
-                <div class="flex items-center gap-3">
-                    <div class="flex items-center justify-center w-16 h-16">
-                        <img class="w-full h-full rounded-full"
-                            src="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/media/Nft6.9ff5403226e81a6fd390.png"
-                            alt="" />
-                    </div>
-                    <div class="flex flex-col">
-                        <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                            happyDog09
-                        </h5>
-                    </div>
-                </div>
-                <div class="flex items-center justify-center mt-3 text-navy-700 dark:text-white">
-                    <button
-                        class="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-semibold text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <div
-                class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
-                <div class="flex items-center gap-3">
-                    <div class="flex items-center justify-center w-16 h-16">
-                        <img class="w-full h-full rounded-full"
-                            src="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/media/Nft4.5fc37877b25c9fb9a52d.png"
-                            alt="" />
-                    </div>
-                    <div class="flex flex-col">
-                        <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                            ramenski
-                        </h5>
-                    </div>
-                </div>
-                <div class="flex items-center justify-center mt-3 text-navy-700 dark:text-white">
-                    <button
-                        class="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-semibold text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <div
-                class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
-                <div class="flex items-center gap-3">
-                    <div class="flex items-center justify-center w-16 h-16">
-                        <img class="w-full h-full rounded-full"
-                            src="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/media/Nft3.3b3e6a4b3ada7618de6c.png"
-                            alt="" />
-                    </div>
-                    <div class="flex flex-col">
-                        <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                            imwriter_s
-                        </h5>
-                    </div>
-                </div>
-                <div class="flex items-center justify-center mt-3 text-navy-700 dark:text-white">
-                    <button
-                        class="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-semibold text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <div
-                class="flex h-full w-full items-start justify-between rounded-md border-[1px] border-[transparent] dark:hover:border-white/20 bg-white px-3 py-[20px] transition-all duration-150 hover:border-gray-200 dark:!bg-navy-800 dark:hover:!bg-navy-700">
-                <div class="flex items-center gap-3">
-                    <div class="flex items-center justify-center w-16 h-16">
-                        <img class="w-full h-full rounded-full"
-                            src="https://horizon-tailwind-react-corporate-7s21b54hb-horizon-ui.vercel.app/static/media/Nft5.62dbaf7dd91b4180035c.png"
-                            alt="" />
-                    </div>
-                    <div class="flex flex-col">
-                        <h5 class="text-base font-bold text-navy-700 dark:text-white">
-                            cute0
-                        </h5>
-                    </div>
-                </div>
-                <div class="flex items-center justify-center mt-3 text-navy-700 dark:text-white">
-                    <button
-                        class="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-semibold text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                    </svg>
-                </button>
-            </div>
         </div>
     </div>
-
-</div></template>
+</template>
 <script setup>
-    import SubscribeTab from '@/components/myPage/subscribeTab.vue'
+import SubscribeTab from '@/components/myPage/subscribeTab.vue';
+import { onMounted, ref,watch } from 'vue';
+import axios from "axios";
+import { appStore } from '@/stores/store';
+import { storeToRefs } from 'pinia'
+const store = appStore()
+const { dataFromChild, setDataFromChild } = store
+const props = defineProps({
+    user: {
+        type: Object,
+        required: true,
+    },
+});
+const subscriberList = ref([]);
+const listSubscriber = async () => {
+    if(store.dataFromChild=='구독자'){
+        await axios.get('http://localhost:8088/users/1/subscriber')
+        .then(response => {
+            // 성공 처리
+            subscriberList.value = response.data;
+            console.log(subscriberList.value);
+            console.log("구독자 불러오기 완료");
+        })
+        .catch(error => {
+            // 에러 처리
+            console.log(error);
+        });
+    }
+    else if(store.dataFromChild=='구독작가'){
+        await axios.get('http://localhost:8088/users/1/subscribedWriter')
+        .then(response => {
+            // 성공 처리
+            subscriberList.value = response.data;
+            console.log(subscriberList.value);
+            console.log("구독 작가 불러오기 완료");
+        })
+        .catch(error => {
+            // 에러 처리
+            console.log(error);
+        });
+    }
+    
+};
+const subscribe = async (userId) => {
+    await axios.get(`http://localhost:8088/users/${userId}/subscribe`)
+        .then(response => {
+            if (response.status === 201) {
+                console.log("구독완료");
+            }
+        })
+        .catch(error => {
+            // 에러 처리
+            console.log(error);
+        });
+};
+watch(() => store.dataFromChild, listSubscriber);
+onMounted(() => {
+	listSubscriber();
+});
 </script>
