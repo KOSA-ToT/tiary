23947fa1fb6e9923b863ef4ba10a8a8b0e2f07fa
ckaanf <skywlstn777@gmail.com>
2023-12-13 14:30:49 +0900

    [SCRIM-108]-feat: 메인 화면 조정 및 게시물 화면 구현
    - SCRUM-96
    - SCRUM-97
    
    메인 페이지 간단한 조정과 게시물 리스트 확인 페이지 구현 ..ing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/pages/Home.vue'
+import ArticleList from '@/pages/ArticleList.vue'
+import ArticlePost from '@/components/ArticlePost.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: Home},
+        {path: '/article', component : ArticleList},
+        {path: '/article-post', component : ArticlePost},
+        {path: '/category' ,name: 'CategoryPage', component :       ArticleList , props : (route) =>
+        ({categoryCode : route.query.categoryCode})}
     ],
     })
 
