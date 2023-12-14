dca82a35d3c5011ea711372b4365842e90a4946d
YSHeee <scotchbroom0@kangwon.ac.kr>
2023-12-14 12:19:57 +0900

    [SCRUM-62] feat: ERROR 페이지 구현 - SCRUM-10
    OAuth2 로그인 실패 시 redirect로 넘어갈 Error 페이지 구현

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/pages/Home.vue'
+import Error from '@/pages/Error.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: Home},
+        {path: '/error', component: Error}
     ],
     })
 
