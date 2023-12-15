0c429d026732a9bc1e993e1bf9f4aa741ab3948e
ckaanf <skywlstn777@gmail.com>
2023-12-15 17:11:36 +0900

    [SCRUM-108]-feat: 게시물 리스트 출력과 게시물 작성 구현 완료
    게시물 리스트 출력시 썸네일 보이게 구현 완료
    없을 시 미 출력
    
    // 로그인 로그아웃 헤더영역에 넣으니 살짝 이슈가 있는데 금방 해결해 보겠습니다..

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,7 +1,6 @@
 <template>
-    <button @click="openModal('로그인')">로그인</button>&nbsp;
-    <button @click="openModal('회원가입')">회원가입</button>&nbsp;
-    <button @click="logout()">로그아웃</button>
+  <Header></Header>
+  
   <div :class="{ dark: isDarkMode, light: !isDarkMode }">
     <Slider></Slider>
     <Category :categories="categories"></Category>
@@ -9,43 +8,22 @@
     <Band></Band>
     <Card></Card>
     <Footer></Footer>
-    <UserModal
-        v-if="userVars.isShowModal"
-        v-model:task="userVars.task"
-        v-model:isShowModal="userVars.isShowModal"
-        >
-    </UserModal>
+    
   </div>
 </template>
 
 <script setup>
+import Header from '@/components/Header.vue'
 import Slider from '@/components/Slider.vue'
 import Footer from '@/components/Footer.vue'
 import Thumbnails from '@/components/Thumbnails.vue'
 import Card from '@/components/Card.vue'
 import Band from '@/components/Band.vue'
-import UserModal from '@/components/UserModal.vue'
+
 import Category from '@/components/Category.vue'
 
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
-
-// 유저
-const userVars = ref({
-    task: '',
-    isShowModal: false
-})
-
-function openModal(tasks) { 
-    userVars.value.task = tasks
-    userVars.value.isShowModal = true;
-}
-
-function logout() {
-  if(confirm("로그아웃 하시겠습니까?")) {
-    localStorage.removeItem('Authorization');
-  }
-}
 
 const categories = ref([])
 onMounted(async () => {
