d4aee621c6651fa25966a9717a55de66d863a382
Nohkii <120151129+Nohkii@users.noreply.github.com>
2023-12-23 16:23:39 +0900

    [SCRUM-86] feat: Admin controller, service, dto, repository추가
    [SCRUM-86] feat: admin controller, service, dto, repository 파일 추가

---
+++ server/src/main/java/com/example/tiary/admin/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.example.tiary.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/admin")
+@RestController
+public class AdminController {
+	// 작가 신청
+
+	// 공지사항
+}
---
+++ server/src/main/java/com/example/tiary/admin/service/AdminService.java
@@ -0,0 +1,10 @@
+package com.example.tiary.admin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+	// 작가 신청
+
+	// 공지사항
+}
