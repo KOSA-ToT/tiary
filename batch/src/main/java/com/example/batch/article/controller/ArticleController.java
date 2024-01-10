77a2233e5013abf9e1b0df9d05fda04af487a530
ckaanf <skywlstn777@gmail.com>
2024-01-10 19:03:11 +0900

    #87- 연관게시물 캐시 관련 수정
    - 서비스 로직 변화에 따라 캐시 컨트롤러 수정
    - API 통신 변화에 따라 dto 수정
    -

--- batch/src/main/java/com/example/batch/article/controller/ArticleController.java
+++ batch/src/main/java/com/example/batch/article/controller/ArticleController.java
@@ -1,15 +1,21 @@
 package com.example.batch.article.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.batch.article.dto.ArticleDto;
 import com.example.batch.article.dto.ResponseArticleDto;
+import com.example.batch.article.entity.Article;
+import com.example.batch.article.entity.RelatedArticle;
 import com.example.batch.article.service.ArticleService;
 import com.example.batch.article.service.ExtractKeywordService;
 import com.example.batch.article.service.RecommendationService;
@@ -25,22 +31,27 @@ public class ArticleController {
 	private final RecommendationService recommendationService;
 	private final ExtractKeywordService extractKeywordService;
 
-
 	@GetMapping("/recommendations/{article-id}")
-	public ResponseEntity getRelatedPost(@PathVariable("article-id") Long articleId){
-		extractKeywordService.calculateAndSaveKeywords(articleId);
-		recommendationService.calculateAndCacheRecommendations(articleId);
+	public ResponseEntity getRelatedPost(@PathVariable("article-id") Long articleId) {
+
+		// List<RelatedArticle> relatedArticles = recommendationService.getRecommendationsFromCache(articleId);
 
 		ResponseArticleDto article = articleService.getRelatedArticle(articleId);
 		return new ResponseEntity(article, HttpStatus.OK);
 	}
 
 	@PostMapping("/updateRecommendations/{article-id}")
-	public ResponseEntity updateRelatedPost(@PathVariable("article-id") Long articleId){
+	public ResponseEntity updateRelatedPost(@PathVariable("article-id") Long articleId,
+		@RequestBody(required = false) ArticleDto articleDto) {
+		if (articleDto != null) {
+			extractKeywordService.calculateAndSaveKeywords(articleId, articleDto.getTitle(), articleDto.getContent());
+		}
+		recommendationService.calculateAndCacheRecommendations(articleId);
 		return null;
 	}
+
 	@GetMapping("/do")
-	public void doRecommend(){
+	public void doRecommend() {
 		extractKeywordService.calculateAndSaveKeywords();
 	}
 }
--- batch/src/main/java/com/example/batch/article/dto/ArticleDto.java
+++ batch/src/main/java/com/example/batch/article/dto/ArticleDto.java
@@ -9,11 +9,13 @@ import lombok.Getter;
 @Builder
 public class ArticleDto {
 	private Long id;
+	private String title;
 	private String content;
 
 	public static ArticleDto of(Article article) {
 		return ArticleDto.builder()
 			.id(article.getId())
+			.title(article.getTitle())
 			.content(article.getContent())
 			.build();
 	}
